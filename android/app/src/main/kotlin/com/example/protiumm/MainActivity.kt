41c603092390fab67046bda466cfc220d4daade8
akshay <akshaymbhat13@gmail.com>
2022-04-13 21:03:53 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/protiumm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.protiumm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,178 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "upload any file",
+      theme: ThemeData(
+        primaryColor: Colors.greenAccent[700],
+      ),
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  File? file;
+  ImagePicker image = ImagePicker();
+  String url = "";
+  var name;
+  var color1 = Colors.redAccent[700];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text(
+          "File Upload",
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              " File upload to firebase backend which supports uploading of files more than 30MB to 150MB ",
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.black,
+                // backgroundColor: Colors.redAccent[700],
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            MaterialButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              elevation: 5.0,
+              height: 60,
+              onPressed: () {
+                getfile();
+              },
+              child: Text(
+                "  Get file",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+              color: Colors.blue,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            MaterialButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20.0),
+                ),
+              ),
+              elevation: 5.0,
+              height: 60,
+              onPressed: () {
+                getImage();
+              },
+              child: Text(
+                "  Get image",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+              color: Colors.blue,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  getfile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf', 'doc', 'docx' 'mp4', 'apk'],
+    );
+
+    if (result != null) {
+      File c = File(result.files.single.path.toString());
+      setState(() {
+        file = c;
+        name = result.names.toString();
+      });
+      uploadFile();
+    }
+  }
+
+  getImage() async {
+    var img = await image.pickImage(source: ImageSource.gallery);
+    setState(() {
+      file = File(img!.path);
+    });
+    if (file != null) {
+      uploadFile();
+    }
+  }
+
+  uploadFile() async {
+    try {
+      var imagefile =
+          FirebaseStorage.instance.ref().child("Users").child("/$name");
+      UploadTask task = imagefile.putFile(file!);
+      TaskSnapshot snapshot = await task;
+      url = await snapshot.ref.getDownloadURL();
+
+      print(url);
+      if (url != null && file != null) {
+        Fluttertoast.showToast(
+          msg: "Done Uploading",
+          textColor: Colors.green,
+        );
+      } else {
+        Fluttertoast.showToast(
+          msg: "Something went wrong",
+          textColor: Colors.red,
+        );
+      }
+    } on Exception catch (e) {
+      Fluttertoast.showToast(
+        msg: e.toString(),
+        textColor: Colors.red,
+      );
+    }
+  }
+}
